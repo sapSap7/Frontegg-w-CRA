573b7c39d74680190ecace45ec5857385c223be1
sapSap7 <sapir280@gmail.com>
2025-06-29 14:45:03 +0300

    frontegg

--- front/front/src/App.js
+++ front/front/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import { useAuth, useLoginWithRedirect, ContextHolder } from "@frontegg/react";
 
 function App() {
+  const { user, isAuthenticated } = useAuth();
+  const loginWithRedirect = useLoginWithRedirect();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, loginWithRedirect]);
+
+  const logout = () => {
+    const baseUrl = ContextHolder.getContext().baseUrl;
+    window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location.href}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated ? (
+        <div>
+          <div>
+            <img src={user?.profilePictureUrl} alt={user?.name} />
+          </div>
+          <div>
+            <span>Logged in as: {user?.name}</span>
+          </div>
+          <div>
+            <button onClick={() => alert(user.accessToken)}>
+              What is my access token?
+            </button>
+          </div>
+          <div>
+            <button onClick={() => logout()}>Click to logout</button>
+          </div>
+          <div>
+            <button
+              onClick={() =>
+                (window.location.href =
+                  "https://portal.frontegg.com/builder/admin-portal")
+              }
+            >
+              Settings
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => loginWithRedirect()}>Click me to login</button>
+        </div>
+      )}
     </div>
   );
 }
--- front/front/src/index.js
+++ front/front/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { FronteggProvider } from "@frontegg/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const contextOptions = {
+  baseUrl: "https://app-x5zh3gpfnerk.frontegg.com",
+  clientId: "7d59c9ae-0cd0-43b0-ad17-c8cc0731dbe0",
+  appId: "051420a5-79bd-4338-a3c1-2c22f626a398",
+};
+
+const authOptions = {
+  keepSessionAlive: true, // Uncomment this in order to maintain the session alive
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container); // only pass it here
+
 root.render(
-  <React.StrictMode>
+  <FronteggProvider
+    contextOptions={contextOptions}
+    hostedLoginBox={true}
+    authOptions={authOptions}
+  >
     <App />
-  </React.StrictMode>
+  </FronteggProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
